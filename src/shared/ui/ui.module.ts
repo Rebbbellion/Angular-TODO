eef4f9dd7f5c785f8d3985b9b1224cc7d2bd5f38
Maksym Dzhosan <dzhomaks@gmail.com>
2024-10-18 20:36:14 +0300

    feat: implement ui and integrate to app

--- src/shared/ui/ui.module.ts
+++ src/shared/ui/ui.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'shared/lib';
+import { HeaderComponent } from './header';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [SharedModule],
-  exports: [],
+  exports: [HeaderComponent],
 })
 export class UiModule {}
