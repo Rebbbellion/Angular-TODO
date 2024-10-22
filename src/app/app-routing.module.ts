66e36d5c3468cb68e44e67d0a82ee255e99358b2
Maksym Dzhosan <dzhomaks@gmail.com>
2024-10-22 23:15:40 +0300

    feat: implement task-list and add tasks
    - Add route to a task list component
    - Add taskService to a task list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from 'pages/task-list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TaskListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
