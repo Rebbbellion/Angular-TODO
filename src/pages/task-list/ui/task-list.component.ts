c68655075c1e63f9ffaaad6fc2b6624da7611808
Maksym Dzhosan <dzhomaks@gmail.com>
2024-11-02 21:08:46 +0200

    feat: implement fadeInOut animation

--- src/pages/task-list/ui/task-list.component.ts
+++ src/pages/task-list/ui/task-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Task, TaskService } from 'entities/task';
+import { fadeInOut } from 'shared/lib';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss',
+  animations: [fadeInOut],
 })
 export class TaskListComponent implements OnInit {
   private readonly data: TaskService = inject(TaskService);
--- src/shared/ui/popup/popup.component.ts
+++ src/shared/ui/popup/popup.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { fadeInOut } from 'shared/lib';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.scss',
+  animations: [fadeInOut],
+  host: {
+    '[@fadeInOut]': '',
+  },
 })
 export class PopupComponent {}
